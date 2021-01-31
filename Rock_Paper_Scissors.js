a6af133bfa2e6e0f5c383597e51cf119bc379161
Marouane Ougnou <mougnou@e1r4p10.1337.ma>
2021-01-31 15:06:24 +0000

    Pushing

---
+++ Rock_Paper_Scissors.js
@@ -0,0 +1,47 @@
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper')
+  {
+    return userInput;
+  }
+  else
+    console.log('Error');
+}
+
+const getComputerChoice = () => {
+  let randomNumber = Math.floor(Math.random() * 3);
+
+  if (randomNumber === 2)
+    return 'rock';
+  else if (randomNumber === 1)
+    return 'paper';
+  else
+    return 'scissors'
+}
+
+
+const determineWinner = (userChoice,computerChoice) => {
+
+  if (userChoice === computerChoice)
+    return ('It\'s a tie!');
+
+  if (userChoice === 'paper' && computerChoice === 'scissors')
+    return ('CPU chose Scissors, Computer Won!');
+
+  else if (userChoice === 'rock' && computerChoice === 'paper')
+    return ('CPU chose Papers, Computer Won!');
+
+  else if (userChoice === 'scissors' && computerChoice === 'rock')
+    return ('CPU chose Rock, Computer Won!');
+    else
+      return ('User won!');
+}
+
+const playGame = (input) => {
+    let computerChoice = getComputerChoice();
+    let userChoice = getUserChoice(input);
+
+    console.log(determineWinner(userChoice,computerChoice));
+}
+
+playGame('rock');
