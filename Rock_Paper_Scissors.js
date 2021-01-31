b050c5e5abcb1816c686e8fc01ede01f33e502e1
MaroIsLife <ougnou10@gmail.com>
2021-01-31 16:13:43 +0100

    Update Rock_Paper_Scissors.js

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -1,47 +1,56 @@
 const getUserChoice = (userInput) => {
-  userInput = userInput.toLowerCase();
-  if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper')
-  {
-    return userInput;
-  }
-  else
-    console.log('Error');
+	userInput = userInput.toLowerCase();
+	if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper')
+	{
+		return userInput;
+	}
+	else if (userInput === 'bomb')
+	{
+		console.log('Bomb Activated');
+		return userInput;
+	}
+	else
+		console.log('Error');
 }
 
 const getComputerChoice = () => {
-  let randomNumber = Math.floor(Math.random() * 3);
+	let randomNumber = Math.floor(Math.random() * 3);
 
-  if (randomNumber === 2)
-    return 'rock';
-  else if (randomNumber === 1)
-    return 'paper';
-  else
-    return 'scissors'
+	if (randomNumber === 2)
+		return 'rock';
+	else if (randomNumber === 1)
+		return 'paper';
+	else
+		return 'scissors'
 }
 
 
 const determineWinner = (userChoice,computerChoice) => {
 
-  if (userChoice === computerChoice)
-    return ('It\'s a tie!');
+	if (userChoice === computerChoice)
+		return ('It\'s a tie!');
 
-  if (userChoice === 'paper' && computerChoice === 'scissors')
-    return ('CPU chose Scissors, Computer Won!');
+	else if (userChoice === 'bomb')
+		return ('User used Bomb, User won!');
 
-  else if (userChoice === 'rock' && computerChoice === 'paper')
-    return ('CPU chose Papers, Computer Won!');
+	else if (userChoice === 'paper' && computerChoice === 'scissors')
+		return ('CPU chose Scissors, Computer Won!');
 
-  else if (userChoice === 'scissors' && computerChoice === 'rock')
-    return ('CPU chose Rock, Computer Won!');
-    else
-      return ('User won!');
+	else if (userChoice === 'rock' && computerChoice === 'paper')
+		return ('CPU chose Papers, Computer Won!');
+
+	else if (userChoice === 'scissors' && computerChoice === 'rock')
+		return ('CPU chose Rock, Computer Won!');
+
+	else
+		return ('User won!');
 }
 
 const playGame = (input) => {
-    let computerChoice = getComputerChoice();
-    let userChoice = getUserChoice(input);
+	let computerChoice = getComputerChoice();
+	let userChoice = getUserChoice(input);
 
-    console.log(determineWinner(userChoice,computerChoice));
+	console.log(determineWinner(userChoice,computerChoice));
 }
 
-playGame('rock');
+playGame('bomb');
